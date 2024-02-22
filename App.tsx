4dc2649d6f595def7a0a6e43eefb5b0875f2f03e
Andriani25 <andriani.leandro25@gmail.com>
2024-02-22 16:53:09 -0300

    ResumeCard ready

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { View} from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import Home from './src/views/Home';
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Home from "./src/views/Home";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
-      <View style={{flex:1}} >
-      <StatusBar style="auto" />
-      <Home/>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Home />
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
-   
   );
 }
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    minHeight: 500,
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+});
--- src/components/ResumeCard.tsx
+++ src/components/ResumeCard.tsx
@@ -1,35 +1,116 @@
-import Rect, {FC} from "react"
-import { View, Text, StyleSheet } from "react-native"
+import Rect, { FC } from "react";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import { Button } from "@rneui/themed";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const ResumeCard: FC = function(){
-    return(
-        <View style={{flex: 1}} >
-            <View style={styles.textContainer} >
-        <Text style={styles.title} >Hi, i'm Leandro</Text>
-        <Text style={styles.text} >{<Text style={[styles.text, {color: "yellow"}]} >With +1 year programming</Text>} and i'm right now interested in {<Text style={[styles.text, {color: "yellow"}]} >React Native + Expo + Expo Web</Text>}</Text>
-            </View>
-        </View>
-    )
-}
+const ResumeCard: FC = function () {
+  return (
+    <View style={{ flex: 1, paddingTop: "20%" }}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.avatarProfile}
+          source={require("../../assets/dev.png")}
+        />
+
+        {windowWidht > 600 ? (
+          <Button
+            size="lg"
+            title="Open to work!"
+            icon={
+              <Ionicons
+                name="logo-linkedin"
+                color="white"
+                size={20}
+                style={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{
+              height: 40,
+              width: 175,
+              borderRadius: 10,
+              marginLeft: 40,
+            }}
+            titleStyle={{
+              fontSize: 15,
+              fontWeight: "bold",
+            }}
+          />
+        ) : (
+          <Button
+            size="lg"
+            icon={<Ionicons name="logo-linkedin" color="white" size={15} />}
+          />
+        )}
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Hi, i'm Leandro</Text>
+        <Text style={styles.text}>
+          I have{" "}
+          {
+            <Text style={[styles.text, { color: "yellow" }]}>
+              +1 year programming
+            </Text>
+          }{" "}
+          and actually interested in{" "}
+          {
+            <Text style={[styles.text, { color: "yellow" }]}>
+              React Native + Expo + Expo Web
+            </Text>
+          }
+        </Text>
+        <Text style={styles.text}>
+          {" "}
+          Born in Argentina, living in Spain right now{" "}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const windowWidht = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+console.log(windowHeight, windowWidht);
 
 const styles = StyleSheet.create({
-    textContainer: {
+  imageContainer: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  leftContainer: {
+    flex: 1,
+  },
+  textContainer: {
     flexDirection: "column",
-    marginTop: 50,
-    backgroundColor: "red",
-    width: "100%",
-    height:"auto"
-    },
-    title: {
-    fontSize: 28,
+    marginTop: windowHeight > 800 ? 20 : 30,
+    backgroundColor: "black",
+  },
+  title: {
+    fontSize: windowWidht > 600 ? 60 : 40,
     fontWeight: "bold",
-    color: "white"
-    },
-    text: {
-        fontSize: 16,
-        fontWeight:"600",
-        color: "white"
-    }
-})
+    color: "white",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "white",
+  },
+  avatarProfile: {
+    borderRadius: 1000,
+    borderWidth: 3,
+    borderColor: "black",
+    height: windowHeight > 800 ? 175 : 100,
+    width: windowWidht > 500 ? 175 : 100,
+  },
+  linkedinIcon: {
+    paddingHorizontal: 10,
+    height: 10,
+    width: 10,
+  },
+});
 
-export default ResumeCard
+export default ResumeCard;
